7bcfbe0449abb51b6808b263f70f89d8ebbaaf20
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-09-14 08:59:56 +0530

    Parsing Double

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fileLines = {
+            "33 0.573140941467E-01 0.112914262390E-03 0.255553577735E-02 0.497192659486E-04 0.141869181079E-01-0.147813598922E-03",
+            "34 0.570076593453E-01 0.100112550891E-03 0.256427138318E-02-0.868691490164E-05 0.142821920093E-01-0.346011975369E-03",
+            "35 0.715507714946E-01 0.316132133031E-03-0.106581466521E-01-0.920513736900E-04 0.138018668842E-01-0.212219497066E-03"
+        };
+
+            var rex = new Regex(@"\b([-+]?\d+(?:\.\d+(?:E[+-]\d+)?)?)\b", RegexOptions.Compiled);
+            foreach (var line in fileLines)
+            {
+
+                var dblValues = new List<double>();
+                foreach (Match match in rex.Matches(line))
+                {
+                    string strVal = match.Groups[1].Value;
+                    double number = Double.Parse(strVal, NumberFormatInfo.InvariantInfo);
+                    dblValues.Add(number);
+                }
+
+                Console.WriteLine(string.Join("; ", dblValues));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = @"h.txt";
+            Func<string, string[]> split = (s) =>
+            {
+                string[] res = new string[7];
+                res[0] = s.Substring(0, 2);
+                for (int i = 0; i < 6; i++)
+                {
+                    res[i + 1] = s.Substring(2 + (i * 19), 19);
+                }
+                return res;
+            };
+            var result = from l in File.ReadAllLines(filename)
+                         let la = split(l)
+                         select new
+                         {
+                             i = int.Parse(la[0]),
+                             d1 = double.Parse(la[1]),
+                             d2 = double.Parse(la[2]),
+                             d3 = double.Parse(la[3]),
+                             d4 = double.Parse(la[4]),
+                             d5 = double.Parse(la[5]),
+                             d6 = double.Parse(la[6])
+
+                         };
+            foreach (var e in result)
+            {
+                Console.WriteLine($"{e.i}, {e.d1}, {e.d2}, {e.d3}, {e.d4}, {e.d5}, {e.d6}");
+            }
+        }
+    }
+}
